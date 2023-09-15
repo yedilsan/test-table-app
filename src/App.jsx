81d5d87a673e235cfc7d667cb65e97245879bd94
yedilsan <yedilsanatovv@gmail.com>
2023-09-15 19:24:22 +0600

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import "./App.css";
 import DataTable from "./components/TableData/DataTable";
-// eslint-disable-next-line no-unused-vars
-import React from "react";
 
 const App = () => (
   <div>
